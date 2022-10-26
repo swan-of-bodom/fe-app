dca0e2fd0f6fd2e764dd26d928e62f132b5ac2e4
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-10-26 18:25:46 +0200

    chore: add redux-logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { debug } from "../utils/debugger";
 import { optionsList } from "./reducers/optionsList";
+import { logger } from "redux-logger";
 
 export const store = configureStore({
   reducer: optionsList.reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
-// Can still subscribe to the store
-store.subscribe(() =>
-  debug("Redux store updated", { state: store.getState() })
-);
