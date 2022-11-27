90b5329f0403920290cf76b248c002a66f1254af
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-11-27 20:33:49 +0100

    feat: move loading button to the page component

--- src/components/PositionTable/PositionTable.tsx
+++ src/components/PositionTable/PositionTable.tsx
@@ -1,36 +1,25 @@
 import { isNonEmptyArray } from "../../utils/utils";
 import {
   Box,
-  Button,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
-  Tooltip,
 } from "@mui/material";
-import { Cached } from "@mui/icons-material";
 import { useAccount } from "@starknet-react/core";
-import { useEffect, useReducer, useState } from "react";
 import { PositionItem } from "./PositionItem";
-import { debug } from "../../utils/debugger";
 import { LoadingAnimation } from "../loading";
 import { NoContent } from "../TableNoContent";
 
-import { fetchPositions, initialState, reducer } from "./fetchPositions";
+import { State } from "./fetchPositions";
 
-export const PositionTableComponent = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const [refresh, toggleRefresh] = useState<boolean>(false);
-  const { address, status } = useAccount();
+type Props = {
+  state: State;
+};
 
-  useEffect(() => {
-    if (status === "connected" && address) {
-      debug("Fetching positons");
-      fetchPositions(address, dispatch);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [status, refresh]);
+export const PositionTableComponent = ({ state }: Props) => {
+  const { address } = useAccount();
 
   if (!address)
     return <NoContent text="Connect your wallet to see your positions." />;
@@ -51,14 +40,7 @@ export const PositionTableComponent = () => {
     <Table sx={{ minWidth: 650 }} aria-label="simple table">
       <TableHead>
         <TableRow>
-          <TableCell>
-            Option{" "}
-            <Tooltip title="Refresh your position list">
-              <Button onClick={() => toggleRefresh(!refresh)}>
-                <Cached />
-              </Button>
-            </Tooltip>
-          </TableCell>
+          <TableCell>Option</TableCell>
           <TableCell align="right">Maturity</TableCell>
           <TableCell align="right">Size</TableCell>
           <TableCell align="right">Value</TableCell>
