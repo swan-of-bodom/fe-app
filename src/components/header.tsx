507648271888d2ac93a63769cc006638115e2902
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-12-24 10:50:29 +0100

    fix: max header width

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,9 @@
 import AppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
 import { Link as RouterLink } from "react-router-dom";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { WalletButton } from "./ConnectWallet/Button";
 import { ReactNode } from "react";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { NetworkSwitch } from "./networkSwitch";
 
 type NavLinkProps = {
@@ -40,7 +39,6 @@ export const Header = () => (
     <GlobalStyles
       styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
     />
-    <CssBaseline />
     <AppBar
       position="static"
       color="default"
@@ -48,23 +46,33 @@ export const Header = () => (
       sx={{
         borderBottom: (theme) => `1px solid ${theme.palette.divider}`,
         padding: 1,
-        flexFlow: "row",
         justifyContent: "center",
         alignItems: "center",
-        flexWrap: "wrap",
       }}
     >
-      <RouterLink to="/" style={{ marginRight: "auto" }}>
-        <img
-          width="47.5px"
-          height="47.5px"
-          src="/logo.svg"
-          alt="Carmine logo"
-        />
-      </RouterLink>
-      <NetworkSwitch />
-      {navLinks.map((navData, i) => navLink(navData, i))}
-      <WalletButton />
+      <Box
+        sx={{
+          display: "flex",
+          flexFlow: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          flexWrap: "wrap",
+          maxWidth: "1250px",
+          width: "100%",
+        }}
+      >
+        <RouterLink to="/" style={{ marginRight: "auto" }}>
+          <img
+            width="47.5px"
+            height="47.5px"
+            src="/logo.svg"
+            alt="Carmine logo"
+          />
+        </RouterLink>
+        <NetworkSwitch />
+        {navLinks.map((navData, i) => navLink(navData, i))}
+        <WalletButton />
+      </Box>
     </AppBar>
   </>
 );
