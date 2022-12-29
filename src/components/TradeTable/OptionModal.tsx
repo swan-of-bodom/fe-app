0e07aab05b301fe18ed625fff75d898e974dd629
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-12-29 19:13:47 +0100

    feat: add theme switching

--- src/components/TradeTable/OptionModal.tsx
+++ src/components/TradeTable/OptionModal.tsx
@@ -11,6 +11,7 @@ import {
   TableRow,
   TextField,
   Typography,
+  useTheme,
 } from "@mui/material";
 import { useAccount } from "@starknet-react/core";
 import BN from "bn.js";
@@ -26,6 +27,7 @@ import { getProfitGraphData } from "../CryptoGraph/profitGraphData";
 import { fetchModalData } from "./fetchModalData";
 import { handleNumericChangeFactory } from "../../utils/inputHandling";
 import { longInteger } from "../../utils/computations";
+import { ThemeVariants } from "../../style/themes";
 
 type ModalProps = {
   open: boolean;
@@ -49,16 +51,6 @@ export type FinancialData = {
   basePremiaEth: number;
   ethInUsd: number;
 } | null;
-
-const style = {
-  position: "absolute" as "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  padding: 2,
-  minWidth: "min(500px, 95vw)",
-  background: "black",
-};
 
 type ProfitTableProps = {
   loading: boolean;
@@ -317,6 +309,18 @@ const OptionBox = ({ option }: OptionBoxProps) => {
 
 export const OptionModal = ({ open, setOpen, option }: ModalProps) => {
   const handleClose = () => setOpen(false);
+  const theme = useTheme();
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    padding: 2,
+    minWidth: "min(500px, 95vw)",
+    background: theme.palette.mode === ThemeVariants.dark ? "black" : "white",
+    border: `solid 1px ${theme.palette.primary.main}`,
+  };
 
   return (
     <Modal
