77806f29139329957406e5d08515e77c425638c8
DaveVodrazka <dave.vodrazka@gmail.com>
2023-03-30 15:18:56 +0200

    feat: add terms and conditions

---
+++ src/pages/termsAndConditions.tsx
@@ -0,0 +1,67 @@
+import Typography from "@mui/material/Typography";
+import { Box, Button, Link, useTheme } from "@mui/material";
+import { useEffect } from "react";
+
+const storeTermsAndConditions = (
+  check: boolean,
+  rerender: (b: boolean) => void
+) => {
+  window.localStorage.setItem("carmine-terms-&-conditions", "accepted");
+  rerender(!check);
+};
+
+type Props = {
+  rerender: (b: boolean) => void;
+  check: boolean;
+};
+
+const TermsAndConditions = ({ check, rerender }: Props) => {
+  useEffect(() => {
+    document.title = "Terms & Conditions | Carmine Finance";
+  });
+
+  const theme = useTheme();
+
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    gap: 4,
+    textAlign: "center",
+    [theme.breakpoints.up("sm")]: {
+      padding: "0 20vw",
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: "0 20px",
+    },
+  };
+
+  // TODO: master URL when merged
+  const termsUrl =
+    "https://github.com/CarmineOptions/fe-app/blob/development/TermsOfUse.md";
+
+  return (
+    <Box sx={style}>
+      <Typography variant="h4">Terms & Conditions</Typography>
+      <Typography variant="body1">
+        Please take a moment to review our terms and conditions, which govern
+        the use of our service. You can access the{" "}
+        <Link color="inherit" href={termsUrl}>
+          document here
+        </Link>
+        . It's important to read and understand these terms before using our
+        service.
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => storeTermsAndConditions(check, rerender)}
+      >
+        Accept Terms and Conditions
+      </Button>
+    </Box>
+  );
+};
+
+export default TermsAndConditions;
