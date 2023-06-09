764a6f7660664b0bd4f33c624be2851005209617
DaveVodrazka <dave.vodrazka@gmail.com>
2023-06-09 13:58:44 +0200

    feat: proposal 13 vote button

--- src/utils/blockchain.ts
+++ src/utils/blockchain.ts
@@ -1,13 +1,19 @@
 import { Contract } from "starknet";
 import { getTokenAddresses } from "../constants/amm";
-
-import AmmAbi from "../abi/amm_abi.json";
 import { store } from "../redux/store";
+import AmmAbi from "../abi/amm_abi.json";
+import GovernanceAbi from "../abi/amm_abi.json";
 
 export const getMainContract = (): Contract => {
   const provider = store.getState().network.provider;
   const { MAIN_CONTRACT_ADDRESS } = getTokenAddresses();
   return new Contract(AmmAbi, MAIN_CONTRACT_ADDRESS, provider);
+};
+
+export const getGovernanceContract = (): Contract => {
+  const provider = store.getState().network.provider;
+  const { GOVERNANCE_CONTRACT_ADDRESS } = getTokenAddresses();
+  return new Contract(GovernanceAbi, GOVERNANCE_CONTRACT_ADDRESS, provider);
 };
 
 export const afterTransaction = (
