2bec4729e21571bee7ae635b9d233db4165cba60
DaveVodrazka <dave.vodrazka@gmail.com>
2023-10-06 17:41:43 +0200

    fix: header padding

---
+++ src/components/LinksMenu/LinksMenu.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as ThreeDots } from "./icon.svg";
+
+export const LinksMenu = () => {
+  return (
+    <div style={{ display: "flex", marginLeft: "1em", cursor: "pointer" }}>
+      <ThreeDots />
+    </div>
+  );
+};
