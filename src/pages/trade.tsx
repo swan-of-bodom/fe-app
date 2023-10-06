ca05357bd36a237bee47be974e64d4634a413d1a
DaveVodrazka <dave.vodrazka@gmail.com>
2023-10-06 22:28:31 +0200

    feat: add alternative graph

--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -2,12 +2,15 @@ import { useEffect } from "react";
 import TradeTable from "../components/TradeTable";
 import { ComplexGraph } from "../components/CryptoGraph/TradingView";
 import { Layout } from "../components/layout";
+import { AlternativeTradingView } from "../components/CryptoGraph/AlternativeTradingView";
 // import { Vote } from "../components/Vote/Vote";
 
 const TradePage = () => {
   useEffect(() => {
     document.title = "Buy | Carmine Finance";
   });
+
+  const showAlternativeGraph = window.location.search.includes("altgraph");
 
   return (
     <Layout>
@@ -17,7 +20,13 @@ const TradePage = () => {
         Vote on proposal 14
       </Typography>
       <Vote /> */}
-      <ComplexGraph />
+      {showAlternativeGraph ? (
+        <div style={{ width: "100%", height: "500px" }}>
+          <AlternativeTradingView />
+        </div>
+      ) : (
+        <ComplexGraph />
+      )}
       <TradeTable />
     </Layout>
   );
