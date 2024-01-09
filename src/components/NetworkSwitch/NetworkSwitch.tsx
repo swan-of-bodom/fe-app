5545c5ed30ef515fae5387d4d218b876f7809514
DaveVodrazka <dave.vodrazka@gmail.com>
2024-01-10 00:18:02 +0100

    fix: network switch anchor

--- src/components/NetworkSwitch/NetworkSwitch.tsx
+++ src/components/NetworkSwitch/NetworkSwitch.tsx
@@ -1,7 +1,6 @@
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { NetworkName } from "../../types/network";
+import Select from "@mui/material/Select";
 import styles from "./networkswitch.module.css";
 import { NETWORK } from "../../constants/amm";
 
@@ -14,40 +13,37 @@ const Ellipse = () => (
   />
 );
 
-export const NetworkSwitch = () => {
-  const handleChange = (e: SelectChangeEvent) => {
-    const target = e.target.value as NetworkName;
-
-    if (target === NetworkName.Mainnet) {
-      // TODO: change to mainnet.app.carmine.finance when ready
-      window.location.href = "https://app.carmine.finance";
-    }
-    if (target === NetworkName.Testnet) {
-      // TODO: uncomment when ready
-      window.location.href = "https://testnet.app.carmine.finance";
-    }
-  };
-
-  return (
-    <FormControl>
-      <Select
-        id="network-select"
-        value={NETWORK}
-        displayEmpty
-        inputProps={{
-          "aria-label": "Without label",
-          IconComponent: () => null,
-        }}
-        onChange={handleChange}
-        className={styles.button}
-      >
-        <MenuItem value="testnet">
-          Testnet <Ellipse />
-        </MenuItem>
-        <MenuItem value="mainnet">
-          Mainnet <Ellipse />
-        </MenuItem>
-      </Select>
-    </FormControl>
-  );
-};
+export const NetworkSwitch = () => (
+  <FormControl>
+    <Select
+      id="network-select"
+      value={NETWORK}
+      displayEmpty
+      inputProps={{
+        "aria-label": "Without label",
+        IconComponent: () => null,
+      }}
+      className={styles.button}
+    >
+      <MenuItem className={styles.menuitem} value="testnet">
+        <a
+          className={styles.itemlink}
+          href="https://testnet.app.carmine.finance/"
+        >
+          <div>
+            <span>Testnet </span>
+            <Ellipse />
+          </div>
+        </a>
+      </MenuItem>
+      <MenuItem className={styles.menuitem} value="mainnet">
+        <a className={styles.itemlink} href="https://app.carmine.finance/">
+          <div>
+            <span>Mainnet </span>
+            <Ellipse />
+          </div>
+        </a>
+      </MenuItem>
+    </Select>
+  </FormControl>
+);
