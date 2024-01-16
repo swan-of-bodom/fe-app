dd5354a6fe2fe01e4c9c1ea6bdd291054163c1ab
DaveVodrazka <dave.vodrazka@gmail.com>
2024-01-16 13:27:47 +0100

    fix: use Promise.allSettled instead of all

--- src/calls/getUserPoolInfo.ts
+++ src/calls/getUserPoolInfo.ts
@@ -17,6 +17,7 @@ import { UserPoolInfo } from "../classes/Pool";
 import { Contract } from "starknet";
 import AuxAbi from "../abi/aux_abi.json";
 import { provider } from "../network/provider";
+import { isPromiseFulfilled } from "../utils/utils";
 
 const method = AMM_METHODS.GET_USER_POOL_INFOS;
 
@@ -40,11 +41,13 @@ const getUserPoolInfoAuxContract = async (
     AuxContract.call("get_user_pool_info", [address, lpAddress])
   );
 
-  const res = await Promise.all(promises);
+  const settledRequests = await Promise.allSettled(promises);
 
-  console.log("HEEERERERERERE", res);
+  const response = settledRequests
+    .filter(isPromiseFulfilled)
+    .map((r) => r.value);
 
-  const parsed = (res as ResponseUserPoolInfo[]).map((v) =>
+  const parsed = (response as ResponseUserPoolInfo[]).map((v) =>
     parseUserPoolInfo(v)
   );
 
