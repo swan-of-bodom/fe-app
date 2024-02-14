318ad371c04575f9ab1cc6a126e7d98e0b09220b
Ondřej Sojka <ondrej.sojka@gmail.com>
2024-02-14 12:13:52 +0100

    Add proposal 50

--- src/components/Proposal/fetchProposal.ts
+++ src/components/Proposal/fetchProposal.ts
@@ -1,3 +1,3 @@
 import { Proposal } from "../../types/proposal";
 
-export const activeProposal: Proposal[] = [];
+export const activeProposal: Proposal[] = [{id:50, discordLink: "https://discord.com/channels/969228248552706078/1207281829229502464"}];
